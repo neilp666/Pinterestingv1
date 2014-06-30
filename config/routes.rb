c2fdd306fe6377ec8dd6a327ce4d94a950e192ac
neil patel <neil@myindianweddingapp.com>
2014-06-30 15:10:19 +0100

    Added Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Pinterestingv1::Application.routes.draw do
+
+  root "pages#home"
+
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
